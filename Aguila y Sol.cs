b490bc39649372ad4b2afe2789a7e879d89b56f0
Shernandezglez <32920669+Shernandezglez@users.noreply.github.com>
2018-03-03 21:09:51 -0600

    Add files via upload

---
+++ Aguila y Sol.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AguilaySol : MonoBehaviour {
+	
+	public int r;
+
+
+	public void lanzamientoDeMoneda()
+	{
+		r = Random.Range (0,2);
+	}
+
+
+	public int getResultado
+	{
+		get { return r; }
+			
+	}
+
+
+}
---
+++ LanzamientoMoneda.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanzamientoMoneda : MonoBehaviour /*AguilaySol*/ {
+	AguilaySol tirada = new AguilaySol();
+	private Transform mov = null;
+	public float vel;
+
+	// Use this for initialization
+	void Start () {
+		
+			mov = GetComponent<Transform> ();
+			
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Q))
+		{
+			tirada.lanzamientoDeMoneda ();
+
+		
+
+			if (tirada.getResultado == 1) 
+			{
+				mov.rotation = Quaternion.Euler (180, -90, 0);
+				print("Aguila");
+
+
+			} else 
+			{
+				mov.rotation = Quaternion.Euler (0, -90, 0);
+				print ("sello");
+
+			}
+
+		}
+
+		/*if (Input.GetKeyDown (KeyCode.W))
+		{
+			for (int i = 0; i <= 10; i++) 
+			{
+				if (tirada.getResultado == 1) 
+				{
+					print("Aguila");
+				} else {
+					print ("sello");
+				}
+
+			}
+
+
+		}*/
+	}
+}
